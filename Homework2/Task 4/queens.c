0cccc25599fe69d34b6fac780bbdc3e60b868623
Casper Johansson <caspthepro@gmail.com>
2025-02-05 14:21:22 +0100

    Queens finished?

--- Homework2/Task 4/queens.c
+++ Homework2/Task 4/queens.c
@@ -5,18 +5,62 @@
 
     gives the number of possible solutions for solving the N-queens problem
     with N = BOARD_SIZE defined below, if you want to change the N, then change
-    that value and recompile. Same goes for number of threads, set NUM_THREADS 
-    before compiling.
+    that value (BOARD_SIZE) and recompile. Same goes for number of threads, set NUM_THREADS 
+    before compiling and recompile to use more or less threads.
 
     compile using:
 
-        gcc -fopenmp -Og -o queens queens.c
-
-    the -Og is to set the compiler into "optimize debug experience" it is not strictly needed.
+        gcc -fopenmp -o queens queens.c
 
     run using:
         cmd:        queens
         powershell: ./queens
+
+    For the results of the runs, we did runs using a 8x8 board,
+    these runs were so fast that the OMP timer could not measure
+    the time with a high enough resolution, it just resulted 
+    in 0.000 seconds for all numbers of threads.
+
+    Therefore, we decided to do our runs with a larger board size,
+    13x13 as this gave a reasonable time for each run that was still
+    measurable.
+
+    +----------------+----------+--------+
+    | Board size(NxN)| Threads  | Time  |
+    +----------------+----------+--------+
+    | 13            | 1         | 0.959  |
+    | 13            | 1         | 0.928  |
+    | 13            | 1         | 0.942  |
+    | 13            | 1         | 0.829  |
+    | 13            | 1         | 0.832  |
+    | 13            | 2         | 0.658  |
+    | 13            | 2         | 0.593  |
+    | 13            | 2         | 0.627  |
+    | 13            | 2         | 0.528  |
+    | 13            | 2         | 0.531  |
+    | 13            | 4         | 0.407  |
+    | 13            | 4         | 0.396  |
+    | 13            | 4         | 0.470  |
+    | 13            | 4         | 0.436  |
+    | 13            | 4         | 0.393  |
+    | 13            | 8         | 0.282  |
+    | 13            | 8         | 0.299  |
+    | 13            | 8         | 0.299  |
+    | 13            | 8         | 0.344  |
+    | 13            | 8         | 0.299  |
+    +---------------+-----------+--------+
+
+    For 1 thread (sequential) the median time was 0.928.
+
+    For 2 threads, the median time was 0.593 so speedup of 1.565.
+    For 4 threads, the median time was 0.407 so speedup of 2.280.
+    For 2 threads, the median time was 0.299 so speedup of 3.104.
+
+    We use (median sequential time / median parallel time) to get speedup.
+
+    We also made the assumption that "generating all solutions" does not mean
+    that they need to be displayed later, just generated so that it increments a
+    counter, so we know how many solutions there are.
 */
 
 
@@ -24,11 +68,8 @@
 #include <stdio.h>
 
 #include <omp.h>
-#include <time.h>
-#include <stdbool.h>
-#include <sys/time.h>
 
-#define BOARD_SIZE 8
+#define BOARD_SIZE 13
 #define NUM_THREADS 8
 
 typedef enum {
@@ -37,20 +78,6 @@ typedef enum {
 } pos_state;
 
 int nr_of_solutions = 0;
-
-double read_timer()
-{
-    static bool initialized = false;
-    static struct timeval start;
-    struct timeval end;
-    if (!initialized)
-    {
-        gettimeofday(&start, NULL);
-        initialized = true;
-    }
-    gettimeofday(&end, NULL);
-    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-}
 
 void empty_board(pos_state board[BOARD_SIZE][BOARD_SIZE])
 {
@@ -163,7 +190,7 @@ int main()
     pos_state board[BOARD_SIZE][BOARD_SIZE];
     empty_board(board);
 
-    double start_time = read_timer();
+    double start_time = omp_get_wtime();
 
     #pragma omp parallel for
     for(int row = 0; row < BOARD_SIZE; row++)
@@ -173,9 +200,9 @@ int main()
         place_queen(thread_board, row, 0);
         recursive_solve(thread_board, 1);
     }
-    double end_time = read_timer();
+    double end_time = omp_get_wtime();
 
-    printf("Completed in %f\n", end_time-start_time);
+    printf("Completed in %f seconds on %d threads\n", end_time-start_time, NUM_THREADS);
     printf("%d solutions found for %dx%d board\n", nr_of_solutions, BOARD_SIZE, BOARD_SIZE);
 
     return 0;
