7c40a12b4923aef471e0260c8fa7a7adc860b0bc
Filip Boive <klebsiella_chupacabras@aleeas.com>
2025-02-10 13:14:37 +0100

    timer removed

--- Homework2/Task 4/queens.c
+++ Homework2/Task 4/queens.c
@@ -38,20 +38,6 @@ typedef enum {
 
 int nr_of_solutions = 0;
 
-double read_timer()
-{
-    static bool initialized = false;
-    static struct timeval start;
-    struct timeval end;
-    if (!initialized)
-    {
-        gettimeofday(&start, NULL);
-        initialized = true;
-    }
-    gettimeofday(&end, NULL);
-    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-}
-
 void empty_board(pos_state board[BOARD_SIZE][BOARD_SIZE])
 {
     for(int i = 0; i < BOARD_SIZE; i++)
